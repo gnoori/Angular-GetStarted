fcd994685704aa97c8272f8adb0cf6d6f784c2ca
gnoori <58310285+gnoori@users.noreply.github.com>
2023-01-05 11:02:02 -0800

    1 binaries uploaded

--- APM/src/app/product-list/product-list.component.ts
+++ APM/src/app/product-list/product-list.component.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   title: string = 'Product List';
-  
+  imgWidth: number = 20;
+  imgMargin: number = 20;
   products: any[] = [
+    {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2021',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    },
     {
       productId: 2,
       productName: 'Garden Cart',
@@ -28,6 +39,26 @@ export class ProductListComponent {
       price: 8.9,
       starRating: 4.8,
       imageUrl: 'assets/images/hammer.png',
+    },
+    {
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2021',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl: 'assets/images/saw.png',
+    },
+    {
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2020',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.png',
     },
   ];
 }
